05bd0001cee62d2f2d2627d2d9226984608f8ec5
romu70 <r@tisserand.eu>
2017-01-18 15:31:32 +0100

    First version. Doesn't work yet because of an issue within Miniflux.

--- miniflux/README.md
+++ miniflux/README.md
@@ -1,4 +1,9 @@
 # [Miniflux](https://www.miniflux.net/)
 
-Use the official Docker image provided [here](https://hub.docker.com/r/miniflux/miniflux/), works great, while not complete (missed themes and, as time of writing, external DB is not supported yet).
+Docker setup to run Miniflux and store data in a external MySQL/MariaDB database. This image installs the latest Miniflux and the available themes. It must be built using the following arguments:
+* DB_HOST to provide the database host
+* DB_DATABASE to set the name of the database
+* DB_USER to provide the database user account
+* DB_PASSWORD...the password of the user.
 
+Those arguments will modify the Miniflux' config.php file. 
---
+++ miniflux/conf/config.php
@@ -0,0 +1,68 @@
+<?php
+
+// HTTP_TIMEOUT => default value is 20 seconds (Maximum time to fetch a feed)
+define('HTTP_TIMEOUT', '20');
+
+// HTTP_MAX_RESPONSE_SIZE => Maximum accepted size of the response body in MB (default 2MB)
+define('HTTP_MAX_RESPONSE_SIZE', 2097152);
+
+// DATA_DIRECTORY => default is data (writable directory)
+define('DATA_DIRECTORY', 'data');
+
+// FAVICON_DIRECTORY => default is favicons (writable directory)
+define('FAVICON_DIRECTORY', DATA_DIRECTORY.DIRECTORY_SEPARATOR.'favicons');
+
+// FAVICON_URL_PATH => default is data/favicons/
+define('FAVICON_URL_PATH', 'data/favicons');
+
+// Database driver: "sqlite" or "postgres", default is sqlite
+define('DB_DRIVER', 'mysql');
+
+// Database connection parameters when Postgres is used
+define('DB_HOSTNAME', 'db-host');
+define('DB_NAME', 'db-name');
+define('DB_USERNAME', 'db-user');
+define('DB_PASSWORD', 'db-password');
+
+// DB_FILENAME => database file when Sqlite is used
+define('DB_FILENAME', DATA_DIRECTORY.'/db.sqlite');
+
+// Enable/disable debug mode
+define('DEBUG_MODE', false);
+
+// DEBUG_FILENAME => default is data/debug.log
+define('DEBUG_FILENAME', DATA_DIRECTORY.'/debug.log');
+
+// THEME_DIRECTORY => default is themes
+define('THEME_DIRECTORY', 'themes');
+
+// SESSION_SAVE_PATH => default is empty (used to store session files in a custom directory)
+define('SESSION_SAVE_PATH', '');
+
+// PROXY_HOSTNAME => default is empty (make HTTP requests through a HTTP proxy if set)
+define('PROXY_HOSTNAME', '');
+
+// PROXY_PORT => default is 3128 (default port of Squid)
+define('PROXY_PORT', 3128);
+
+// PROXY_USERNAME => default is empty (set the proxy username is needed)
+define('PROXY_USERNAME', '');
+
+// PROXY_PASSWORD => default is empty
+define('PROXY_PASSWORD', '');
+
+// SUBSCRIPTION_CONCURRENT_REQUESTS => number of concurrent feeds to refresh at once
+// Reduce this number on systems with limited processing power
+define('SUBSCRIPTION_CONCURRENT_REQUESTS', 5);
+
+// Disable automatically a feed after X parsing failure
+define('SUBSCRIPTION_DISABLE_THRESHOLD_ERROR', 10);
+
+// Allow the cronjob to be accessible from the browser
+define('ENABLE_CRONJOB_HTTP_ACCESS', true);
+
+// Enable/disable HTTP header X-Frame-Options
+define('ENABLE_XFRAME', true);
+
+// Enable/disable HSTS HTTP header
+define('ENABLE_HSTS', true);
