c7b89da2c30bfa4484a61c72ca8afa1ffd638544
romu70 <r@tisserand.eu>
2017-01-22 17:15:03 +0100

    Bug fix.

--- miniflux/conf/db-setup.sh
+++ miniflux/conf/db-setup.sh
@@ -10,9 +10,9 @@ DB_NAME=$4
 DB_USER=$5
 DB_USER_PWD=$6
 
-sed -i -e "s/db-driver\s*/$DB_DRIVER/g" /var/www/config.php\
-sed -i -e "s/db-host\s*/$DB_HOST/g" /var/www/config.php\
-sed -i -e "s/db-port\s*/$DB_PORT/g" /var/www/config.php\
-sed -i -e "s/db-name\s*/$DB_DATABASE/g" /var/www/config.php\
-sed -i -e "s/db-user\s*/$DB_USER/g" /var/www/config.php\
-sed -i -e "s/db-password\s*/$DB_PASSWORD/g" /var/www/config.php
+sed -i -e "s/db-driver\s*/$DB_DRIVER/g" ./config.php
+sed -i -e "s/db-host\s*/$DB_HOST/g" ./config.php
+sed -i -e "s/db-port\s*/$DB_PORT/g" ./config.php
+sed -i -e "s/db-name\s*/$DB_NAME/g" ./config.php
+sed -i -e "s/db-user\s*/$DB_USER/g" ./config.php
+sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./config.php
