f1f91d99e319f92f1bd867a47955053d320038c0
romu <romu@xps.tisserand>
2017-11-01 18:41:41 +0100

    Fixed some issues.

--- leanote/conf/db-setup.sh
+++ leanote/conf/db-setup.sh
@@ -12,5 +12,6 @@ APP_SECRET=$4
 sed -i -e "s/db-host\s*/$DB_HOST/g" ./app.conf
 sed -i -e "s/db-port\s*/$DB_PORT/g" ./app.conf
 sed -i -e "s/db-user\s*/$DB_USER/g" ./app.conf
+sed -i -e "s/db-pwd\s*/$DB_USER_PWD/g" ./app.conf
 sed -i -e "s/app-secret\s*/$APP_SECRET/g" ./app.conf
 
