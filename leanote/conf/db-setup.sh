5b7ce3280be4a99b3b112f59701d73e105063268
romu <romu@xps.tisserand>
2017-11-06 22:22:21 +0100

    Added the necessary to pass the host address and port when setting up the container.

--- leanote/conf/db-setup.sh
+++ leanote/conf/db-setup.sh
@@ -8,10 +8,13 @@ DB_PORT=$2
 DB_USER=$3
 DB_USER_PWD=$4
 APP_SECRET=$5
+HOST_ADDR=$6
+HOST_PORT=$7
 
 sed -i -e "s/db-host\s*/$DB_HOST/g" ./app.conf
 sed -i -e "s/db-port\s*/$DB_PORT/g" ./app.conf
 sed -i -e "s/db-user\s*/$DB_USER/g" ./app.conf
 sed -i -e "s/db-pwd\s*/$DB_USER_PWD/g" ./app.conf
-sed -i -e "s/app-secret\s*/$APP_SECRET/g" ./app.conf
+sed -i -e "s/host-addr\s*/$HOST_ADDR/g" ./app.conf
+sed -i -e "s/host-port\s*/$HOST_PORT/g" ./app.conf
 
