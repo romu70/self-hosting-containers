65e062e487d8ab2c65b619abb26d98802b1440c9
romu <romu@xps.tisserand>
2018-01-13 17:09:32 +0100

    Added all elements needed to connect the DB.

--- paperwork/conf/db-setup.sh
+++ paperwork/conf/db-setup.sh
@@ -5,10 +5,14 @@
 # Inputs:
 DB_HOST=$1
 DB_PORT=$2
-DB_USER_PWD=$3
+DB_DATABASE=$3
+DB_USER=$4
+DB_USER_PWD=$5
 
 sed -i -e "s/db-host\s*/$DB_HOST/g" ./database.json
 sed -i -e "s/db-port\s*/$DB_PORT/g" ./database.json
+sed -i -e "s/db-database\s*/$DB_DATABASE/g" ./database.json
+sed -i -e "s/db-user\s*/$DB_USER/g" ./database.json
 sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./database.json
 
 php artisan migrate
