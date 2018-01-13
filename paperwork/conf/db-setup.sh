88ae3d753865c4555b700a1d5a6f103b1460905c
romu <romu@xps.tisserand>
2018-01-13 18:30:23 +0100

    Simplified the db setup procedure. The script is now in the frontend folder and add data to the file located elsewhere.

--- paperwork/conf/db-setup.sh
+++ paperwork/conf/db-setup.sh
@@ -9,10 +9,10 @@ DB_DATABASE=$3
 DB_USER=$4
 DB_USER_PWD=$5
 
-sed -i -e "s/db-host\s*/$DB_HOST/g" ./database.json
-sed -i -e "s/db-port\s*/$DB_PORT/g" ./database.json
-sed -i -e "s/db-database\s*/$DB_DATABASE/g" ./database.json
-sed -i -e "s/db-user\s*/$DB_USER/g" ./database.json
-sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./database.json
+sed -i -e "s/db-host\s*/$DB_HOST/g" ./app/storage/config/database.json
+sed -i -e "s/db-port\s*/$DB_PORT/g" ./app/storage/config/database.json
+sed -i -e "s/db-database\s*/$DB_DATABASE/g" ./app/storage/config/database.json
+sed -i -e "s/db-user\s*/$DB_USER/g" ./app/storage/config/database.json
+sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./app/storage/config/database.json
 
 php artisan migrate
