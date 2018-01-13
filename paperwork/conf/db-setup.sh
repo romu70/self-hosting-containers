9b991c100b2129b97e2db55036c991ded2a26373
romu <romu@xps.tisserand>
2018-01-13 14:37:16 +0100

    Switched to new settings file names.

--- paperwork/conf/db-setup.sh
+++ paperwork/conf/db-setup.sh
@@ -7,8 +7,8 @@ DB_HOST=$1
 DB_PORT=$2
 DB_USER_PWD=$3
 
-sed -i -e "s/db-host\s*/$DB_HOST/g" ./default_database.json
-sed -i -e "s/db-port\s*/$DB_PORT/g" ./default_database.json
-sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./default_database.json
+sed -i -e "s/db-host\s*/$DB_HOST/g" ./database.json
+sed -i -e "s/db-port\s*/$DB_PORT/g" ./database.json
+sed -i -e "s/db-password\s*/$DB_USER_PWD/g" ./database.json
 
 php artisan migrate
